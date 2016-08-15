0e15ff08dd863b521be1d186871343d47eff84c7
silence940109 <695412269@qq.com>
2016-08-15 16:53:25 +0800

    resouce code

---
+++ src/HelloJob.java
@@ -0,0 +1,17 @@
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class HelloJob implements Job{
+
+	public void execute(JobExecutionContext context) throws JobExecutionException {
+		System.out.println("hello world begin!");
+		try {
+			Thread.sleep(6000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("hello world end!");
+		
+	}
+}
